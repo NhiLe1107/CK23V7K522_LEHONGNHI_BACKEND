c3cda010b8b7209b2f034cd0a1f2d660caf8117f
NhiLe1107 <nhick23v7k522@vlvh.ctu.edu.vn>
2025-03-18 22:07:28 +0700

    git commit -m "Dinh nghia cac route cho tai nguyen Contact"

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require("express");
 
+const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+// Import route contacts
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
-
 app.use(express.json());
-
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
-
 });
-
+// Đăng ký routes cho contacts
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
